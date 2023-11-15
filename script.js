0584fa2d64cb02246b732f9899a20e5e6dfcce48
xAp0110x <elmar.bashirov13@gmail.com>
2023-11-16 03:13:58 +0400

    final version

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
-let convert = converterStockAPI;
 const moneyFromButtons = document.querySelectorAll(".select-from button");
 const moneyToButtons = document.querySelectorAll(".select-to button");
 const leftRate = document.getElementById('left-rate');
 const rightRate = document.getElementById('right-rate');
+const firstInput = document.getElementById('first-input');
+const secondInput = document.getElementById('second-input');
 
-const apiKey = "e8090bf2230057779ccc1f61f3ac856f";
+const apiKey = "6f4fbdda846674d4de9d6c0f1db52b3c";
 const apiUrl = "http://api.exchangerate.host/convert";
 
-// Convertion func
-async function converterStockAPI(from, to, amount) {
-
-    const url = `${apiUrl}?access_key=${apiKey}&from=${from}&to=${to}&amount=${amount}`;
-    let result;
-
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-
-        return data.result;
-    } catch (error) {
-        console.error("Error:", error);
-        return null;
-    }
-}
-
-// converterStockAPI('RUB', 'USD', 1817)
-//     .then(result => {
-//         console.log(result); 
-//     });
 
 //Update currency rate func
-async function renewCurrencyRates(){
+async function renewCurrencyRates() {
     let firstCurrency = document.querySelector('.select-from button.selected').innerText;
     let secondCurrency = document.querySelector('.select-to button.selected').innerText;
 
@@ -49,21 +29,71 @@ async function renewCurrencyRates(){
     let secondRate = data.info.quote;
 
     leftRate.textContent = `1 ${firstCurrency} = ${firstRate} ${secondCurrency}`;
-    rightRate.textContent = `1 ${secondCurrency} = ${secondRate} ${firstCurrency}`;   
+    rightRate.textContent = `1 ${secondCurrency} = ${secondRate} ${firstCurrency}`;
 }
 renewCurrencyRates();
+
 
 //Selected btn func
 function selectButtonClick(button, buttons) {
     buttons.forEach((btn) => btn.classList.remove("selected"));
     button.classList.add("selected");
     renewCurrencyRates();
+
 }
 //Event click on button
 moneyFromButtons.forEach((button) => {
-    button.addEventListener("click", () => selectButtonClick(button, moneyFromButtons));
+    button.addEventListener("click", async function () {
+        selectButtonClick(button, moneyFromButtons);
+
+        let firstCurrency = String(document.querySelector('.select-from button.selected').innerText);
+        let secondCurrency = String(document.querySelector('.select-to button.selected').innerText);
+        let resultValue = 0;
+
+        const url = `${apiUrl}?access_key=${apiKey}&from=${firstCurrency}&to=${secondCurrency}&amount=${firstInput.value}`;
+        let response = await fetch(url);
+        let data = await response.json();
+
+        if (data.result != undefined) { secondInput.value = data.result.toFixed(4); }
+    });
 });
 moneyToButtons.forEach((button) => {
-    button.addEventListener("click", () => selectButtonClick(button, moneyToButtons));
+    button.addEventListener("click", async function () {
+        selectButtonClick(button, moneyToButtons);
+
+        let firstCurrency = String(document.querySelector('.select-from button.selected').innerText);
+        let secondCurrency = String(document.querySelector('.select-to button.selected').innerText);
+        let resultValue = 0;
+
+        const url = `${apiUrl}?access_key=${apiKey}&from=${secondCurrency}&to=${firstCurrency}&amount=${secondInput.value}`;
+        let response = await fetch(url);
+        let data = await response.json();
+
+        if (data.result != undefined) { firstInput.value = data.result.toFixed(4); }
+    });
 });
 
+
+firstInput.addEventListener('input', async function (event) {
+    let firstCurrency = String(document.querySelector('.select-from button.selected').innerText);
+    let secondCurrency = String(document.querySelector('.select-to button.selected').innerText);
+    let resultValue = 0;
+
+    const url = `${apiUrl}?access_key=${apiKey}&from=${firstCurrency}&to=${secondCurrency}&amount=${firstInput.value}`;
+    let response = await fetch(url);
+    let data = await response.json();
+
+    if (data.result != undefined) { secondInput.value = data.result.toFixed(4); }
+});
+
+secondInput.addEventListener('input', async function (event) {
+    let firstCurrency = String(document.querySelector('.select-from button.selected').innerText);
+    let secondCurrency = String(document.querySelector('.select-to button.selected').innerText);
+    let resultValue = 0;
+
+    const url = `${apiUrl}?access_key=${apiKey}&from=${secondCurrency}&to=${firstCurrency}&amount=${secondInput.value}`;
+    let response = await fetch(url);
+    let data = await response.json();
+
+    if (data.result != undefined) { firstInput.value = data.result.toFixed(4); }
+});
