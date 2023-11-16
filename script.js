32c9f1f6fa9d8a89d07d9f27fc257201631c8465
xAp0110x <elmar.bashirov13@gmail.com>
2023-11-17 01:00:19 +0400

    bugs fixed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const rightRate = document.getElementById('right-rate');
 const firstInput = document.getElementById('first-input');
 const secondInput = document.getElementById('second-input');
 
-const apiKey = "6f4fbdda846674d4de9d6c0f1db52b3c";
+const apiKey = "372d050fb40d114de45b24ef60b1ca3d";
 const apiUrl = "http://api.exchangerate.host/convert";
 
 
@@ -75,6 +75,10 @@ moneyToButtons.forEach((button) => {
 
 
 firstInput.addEventListener('input', async function (event) {
+    if ((event.inputType === 'insertText') && ((event.data === ',') || (event.data === '.'))) {
+        return;
+    }
+
     let firstCurrency = String(document.querySelector('.select-from button.selected').innerText);
     let secondCurrency = String(document.querySelector('.select-to button.selected').innerText);
     let resultValue = 0;
@@ -83,10 +87,15 @@ firstInput.addEventListener('input', async function (event) {
     let response = await fetch(url);
     let data = await response.json();
 
-    if (data.result != undefined) { secondInput.value = data.result.toFixed(4); }
+    if (data.result != null) { secondInput.value = data.result.toFixed(4); }
+    else { secondInput.value = null }
 });
 
 secondInput.addEventListener('input', async function (event) {
+    if ((event.inputType === 'insertText') && ((event.data === ',') || (event.data === '.'))) {
+        return;
+    }
+
     let firstCurrency = String(document.querySelector('.select-from button.selected').innerText);
     let secondCurrency = String(document.querySelector('.select-to button.selected').innerText);
     let resultValue = 0;
@@ -95,20 +104,37 @@ secondInput.addEventListener('input', async function (event) {
     let response = await fetch(url);
     let data = await response.json();
 
-    if (data.result != undefined) { firstInput.value = data.result.toFixed(4); }
+    if (data.result != null) { firstInput.value = data.result.toFixed(4); }
+    else { firstInput.value = null; }
 });
 
 //Check for correct of input and operating with
-function checkInput(event) {
-  let sanitizedValue = event.target.value.replace(/[^0-9.,]/g, '');
 
-  sanitizedValue = sanitizedValue.replace(/,/g, '.');
+firstInput.addEventListener('input', function(event){
+    let sanitizedValue = event.target.value.replace(/[^0-9.,]/g, '');
 
-  let dotCount = sanitizedValue.split('.').length - 1;
-  if (dotCount > 1) {sanitizedValue = sanitizedValue.slice(0, -1);}
 
-  event.target.value = sanitizedValue;
-}
+    sanitizedValue = sanitizedValue.replace(/,/g, '.');
+    let dotCount = sanitizedValue.split('.').length - 1;
+    if (dotCount > 1) { sanitizedValue = sanitizedValue.slice(0, -1); }
 
-firstInput.addEventListener('input', checkInput);
-secondInput.addEventListener('input', checkInput);
+    if (event.target.value[0] == '.' || event.target.value[0] == ',') { 
+        sanitizedValue = null;
+    }
+
+    event.target.value = sanitizedValue;    
+});
+secondInput.addEventListener('input', function(event){
+    let sanitizedValue = event.target.value.replace(/[^0-9.,]/g, '');
+
+
+    sanitizedValue = sanitizedValue.replace(/,/g, '.');
+    let dotCount = sanitizedValue.split('.').length - 1;
+    if (dotCount > 1) { sanitizedValue = sanitizedValue.slice(0, -1); }
+
+    if (event.target.value[0] == '.' || event.target.value[0] == ',') { 
+        sanitizedValue = null;
+    }
+
+    event.target.value = sanitizedValue;
+});
